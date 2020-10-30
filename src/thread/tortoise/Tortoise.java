2d87e9188b454406e992f4ad65ab601c6a2ba494
Ashutosh Pachouri <ashutosh.pachouri_cs19@gla.ac.in>
2020-10-30 17:27:28 +0530

    Overriding run method for Tortoise.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -4,4 +4,12 @@ import javax.swing.*;
 
 public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+
+        }
+    }
 }
+
